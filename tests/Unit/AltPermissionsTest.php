41d9977735f1454aaaef3c713c10e74bde71a6b8
Louis Charette <charette.louis@gmail.com>
2017-04-07 17:47:23 -0400

    Sving WIP

--- tests/Unit/AltPermissionsTest.php
+++ tests/Unit/AltPermissionsTest.php
@@ -3,8 +3,8 @@
 namespace UserFrosting\Tests\Unit;
 
 use UserFrosting\Tests\TestCase;
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
-//use Illuminate\Foundation\Testing\DatabaseTransactions;
+//use UserFrosting\Tests\DatabaseMigrations;
+use UserFrosting\Tests\DatabaseTransactions;
 
 use UserFrosting\Sprinkle\AltPermissions\Model\User;
 use UserFrosting\Sprinkle\AltPermissions\Model\AltRole;
@@ -12,6 +12,38 @@ use UserFrosting\Sprinkle\Gaston\Model\Project;
 
 class AltPermissionsTest extends TestCase
 {
+    use DatabaseTransactions;
+
+    protected $testData;
+
+    protected function setUp()
+    {
+        $this->testData = collect([]);
+
+
+        // Create 2 users
+        /*$user_1 = new User([
+
+        ])->save();*/
+        //$user_2
+
+        // Create 4 projects
+        /*$project_1
+        $project_2
+        $project_3
+        $project_4
+
+        // Create 3 roles
+        $role_1
+        $role_2
+        $role_3
+
+        // Assign them all together
+        */
+
+        parent::setUp();
+    }
+
     public function test__U_S_R()
     {
         $results = collect([]);
@@ -38,6 +70,8 @@ class AltPermissionsTest extends TestCase
                 //Debug::debug($user->id . " -> " . $project->id . " -> " . $role->id . " (" . $project->name . " -> " . $role_name . ")");
             }
         }
+
+        //echo print_r($results, true);
 
         $diff = $results->diff([2, 4, 6, 8]);
         $this->assertEmpty($diff->all(), "U => S -> R Failed");
